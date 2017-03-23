d7ae01e097551b3c6fc56fee8da3f599f89570a0
Viraj Rai <vr2376@columbia.edu>
2017-03-23 17:10:08 -0400

    Suppressed make clean output

--- chat.sh
+++ chat.sh
@@ -4,8 +4,15 @@ if [ -e "$file" ]
 then 
     rm $file
 fi
+
+on_ctrl_c(){
+    echo "Ignoring Ctrl-C"
+}
+
+trap on_ctrl_c INT
+
 make -s
 mkfifo $file
 stdbuf -o 0 ./simchat | nc clac.cs.columbia.edu 10643 | ./virajchat
 rm $file
-make clean
+make -s  clean
